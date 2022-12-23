b8edfeab748ec131bd2c48769cc39d513e6cca30
KONTRA <56182197+ABHISHEK-CPU@users.noreply.github.com>
2022-12-23 20:58:17 +0530

    Update and rename poc.html to poc.php

---
+++ poc.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>PHP in HTML Example</title>
+</head>
+<body>
+	
+	
+		
+		<?php
+			echo "PHP in HTML Example";
+			
+		?>
+
+	<iframe src="https://attacker.com)" width="800" height="800"/>
+</body>
+</html>
